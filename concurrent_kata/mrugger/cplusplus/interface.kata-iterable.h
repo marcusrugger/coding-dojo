ec1abc7038d8f961cb3b6eaa66b1fef3ca1baa31
Marcus Rugger <marcusrugger@yahoo.com>
2014-01-08 07:41:44 -0500

    Removed some old stuff

--- concurrent_kata/mrugger/cplusplus/interface.kata-iterable.h
+++ concurrent_kata/mrugger/cplusplus/interface.kata-iterable.h
@@ -33,24 +33,4 @@ public:
 };
 
 
-class StreamIterable
-{
-public:
-
-  virtual void for_each(std::function<int(SequenceIterable *)> lambda) = 0;
-
-};
-
-
-class KataStream
-{
-public:
-
-  virtual unsigned int readbytes(unsigned int count, char *block) = 0;
-  virtual void rewind(unsigned int count) = 0;
-  virtual bool is_eof(void) = 0;
-
-};
-
-
 #endif  // __INTERFACE_KATA_ITERABLE_H__
